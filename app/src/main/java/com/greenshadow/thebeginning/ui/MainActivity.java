c9017b2e284fc27743b86384f2071717f6665c91
qiushou.liu-nb <qiushou.liu-nb@jrdcom.com>
2016-08-26 22:51:04 +0800

    design

--- app/src/main/java/com/greenshadow/thebeginning/ui/MainActivity.java
+++ app/src/main/java/com/greenshadow/thebeginning/ui/MainActivity.java
@@ -3,6 +3,7 @@ package com.greenshadow.thebeginning.ui;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 
 import com.greenshadow.thebeginning.R;
 
@@ -10,9 +11,13 @@ import com.greenshadow.thebeginning.R;
  * @author greenshadow
  */
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
     }
 }
